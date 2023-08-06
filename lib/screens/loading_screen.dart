acb9534f4b470ba17592092bb05d44e00a6bc65f
Mahmoud <mahmoudabohebil@gmail.com>
2023-08-06 10:11:20 +0300

    Getting the weatherData for current location.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,27 +4,35 @@ import 'package:clima_with_api_flutter/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+const apiKey = "3e186f0092055e392c60e2dd6a2c987d";
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
   @override
   void initState() {
     super.initState();
     getLocation();
-    getData();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+    setState(() {
+      latitude = location.latitude;
+      longitude = location.longitude;
+    });
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=3e186f0092055e392c60e2dd6a2c987d"));
+        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey"));
     if (response.statusCode == 200) {
       String data = response.body;
       var decodedData = jsonDecode(data);
