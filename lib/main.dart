ae42109552f163de08b158b3f836b134eaf47321
Mahmoud <mahmoudabohebil@gmail.com>
2023-08-09 15:26:42 +0300

    Stopping the device form auto rotating is done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:clima_with_api_flutter/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
